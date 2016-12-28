8b57b25ff1fa4459d40b6d0706de597cbe31e2df
i-kiwi <i.kiwi@qq.com>
2016-12-28 18:52:55 +0800

    [整合redis]

--- src/com/taikang/controller/Demo.java
+++ src/com/taikang/controller/Demo.java
@@ -3,11 +3,9 @@ package com.taikang.controller;
 import com.taikang.dao.RedisBaseDao;
 import com.taikang.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import redis.clients.jedis.JedisPoolConfig;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
